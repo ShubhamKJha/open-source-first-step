54df64b7f0043923d084d77651498db47cb4c417
Shubham Kumar Jha <skjha832@gmail.com>
2018-11-03 14:49:25 +0530

    fourth commit

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+// Hello World Program in C++
+// Language: C++
+// by: ShubhamKJha<skjha832@gmail.com>
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello World!"<<endl;
+	return 0;
+}
+
--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,9 @@
+#!/bin/python
+"""
+Hello World Program 
+Language: Python
+By: ShubhamKJha<skjha832@gmail.com>
+"""
 print("Hello World!")
+
 
